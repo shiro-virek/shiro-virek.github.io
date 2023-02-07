71f91c9b8b8caa6550e600a9f3b4829065bcf07f
ALGEIBA\epelroso <epelroso@algeiba.com>
2023-02-07 16:55:00 -0300

    Even more randomized features

--- scripts/art-1.js
+++ scripts/art-1.js
@@ -2,20 +2,22 @@
 	let width = 0;
 	let height = 0;
 	let angle = 0;	
-	let lastPosY = 0;
-	let lastPosX = 0;
+	let lastPosY = null;
+	let lastPosX = null;
+
+	let rotatorsLengths = [];
+	let rotatorsAngles = [];
+
 	let CANVAS_ID = "myCanvas"
 	let RAD_CONST = 0.0175;
 
-	let ROTATE = true;
 	let OPACITY = 0.5;
 	let THICKNESS = 5;
-	let LINE_1_LENGTH = 2;
- 	let LINE_2_LENGTH = 3;
- 	let ANGLE_1 = 10;
- 	let ANGLE_2 = 120;
- 	let ANGLE_3 = 240;
+
+ 	let ANGLE_ROTATION = 10;
+
  	let COLOR_MAP_MAX = 500;
+ 	let ROTATORS = 3;
 
 	getRandomBool = () => {
 		return Math.random() < 0.5;
@@ -25,22 +27,27 @@
 		return Math.floor(Math.random() * max) + min;
 	}
 
-
 	scale = (number, inMin, inMax, outMin, outMax) => {
 	    return (number - inMin) * (outMax - outMin) / (inMax - inMin) + outMin;
 	}
 
-
 	randomize = () => {
-		ROTATE = getRandomBool();
-		OPACITY = getRandomInt(1, 10) / 10;
-		THICKNESS = getRandomInt(3, 100);
-		LINE_1_LENGTH = getRandomInt(1, 10);
-	 	LINE_2_LENGTH = getRandomInt(1, 10);
-	 	ANGLE_1 = getRandomInt(1, 30);
-	 	ANGLE_2 = getRandomInt(1, 360);
-	 	ANGLE_3 = getRandomInt(1, 360);
-	 	COLOR_MAP_MAX =  getRandomInt(1, 5000);
+		OPACITY = getRandomInt(1, 6) / 10;
+		THICKNESS = getRandomInt(1, 30);
+	 	ANGLE_ROTATION = getRandomInt(0, 20);
+	 	COLOR_MAP_MAX = getRandomInt(1, 5000);
+
+	 	ROTATORS = getRandomInt(0, 7);
+
+	 	let rotatorsLength = 0
+	 	let rotatorsAngle = 0;
+
+	 	for (let i = 0; i < ROTATORS - 1; i++){
+	 		rotatorsLength += getRandomInt(1, 1000)
+	 		rotatorsAngle += getRandomInt(1, 180)
+	 		rotatorsLengths.push(rotatorsLength);
+	 		rotatorsAngles.push(rotatorsAngle);
+	 	}
 	}
 
 	init = () => {	
@@ -69,7 +76,7 @@
 			canvas.width = width;
 	  		canvas.height = height;
 			let ctx = canvas.getContext('2d')
-			ctx.fillStyle = "#000000";
+			ctx.fillStyle = "#333";
 			ctx.fillRect(0, 0, canvas.width, canvas.height);
 			ctx.lineWidth = 1;
 			ctx.strokeStyle = '#000000';
@@ -82,6 +89,7 @@
 		if (canvas.getContext){
 			let ctx = canvas.getContext('2d');
 			ctx.lineWidth = width;
+			ctx.lineCap = 'round';
 			ctx.strokeStyle = color;
 			ctx.beginPath();
 			ctx.moveTo(x, y);
@@ -91,44 +99,36 @@
 	}
 			
 	paint = (xPointer, yPointer) => {	
-		const trailFactor = 7;
+		if (lastPosX == 0) lastPosX = xPointer;
+		if (lastPosY == 0) lastPosY = yPointer;
+
 		let distance = Math.sqrt(Math.pow(lastPosX - xPointer, 2) + Math.pow(lastPosY - yPointer, 2))		
 		let hue =  scale(distance, 0, 360, 0, COLOR_MAP_MAX);
 
 		let color = `hsl(${hue}, 100%, 50%, ${OPACITY})`;
-		let colorTrail = `hsl(${hue}, 100%, 50%, ${OPACITY})`;
 
-		let lineWidth = scale(distance, 0, 100, 0, THICKNESS);			
+		let lineWidth = scale(distance, 1, 100, 0, THICKNESS);			
 		
 		this.drawLine(lastPosX, lastPosY, xPointer, yPointer, lineWidth, color);	
-		
-		if (ROTATE){
-			let angleRad = angle * RAD_CONST;
-			let angleRad2 = (angle + ANGLE_2) * RAD_CONST;			
-			let angleRad3 = (angle + ANGLE_3) * RAD_CONST;
+	
+		let angleRad = angle * RAD_CONST;		
 
-			this.drawLine(xPointer, yPointer, 
-				xPointer + parseInt((distance * trailFactor) * Math.cos(angleRad)), 
-				yPointer + parseInt((distance * trailFactor) * Math.sin(angleRad)), 
-				lineWidth, colorTrail);	
+		this.drawLine(xPointer, yPointer, 
+			xPointer + parseInt(distance * Math.cos(angleRad)), 
+			yPointer + parseInt(distance * Math.sin(angleRad)), 
+			lineWidth, color);			
 
-			this.drawLine(xPointer, yPointer, 
-				xPointer + parseInt((distance * trailFactor * LINE_1_LENGTH) * Math.cos(angleRad2)), 
-				yPointer + parseInt((distance * trailFactor * LINE_1_LENGTH) * Math.sin(angleRad2)), 
-				lineWidth, colorTrail);					
-
-			this.drawLine(xPointer, yPointer, 
-				xPointer + parseInt((distance * trailFactor * LINE_2_LENGTH) * Math.cos(angleRad3)), 
-				yPointer + parseInt((distance * trailFactor * LINE_2_LENGTH) * Math.sin(angleRad3)), 
-				lineWidth, colorTrail);	
-			
-			if (angle >= 360 - ANGLE_1) {
-				angle = 0;
-			}else{
-				angle += ANGLE_1;
+		if (ROTATORS > 0)
+			for (let i = 0; i < ROTATORS - 1; i++){
+				let angleRad2 = (angle + rotatorsAngles[i]) * RAD_CONST;	
+				this.drawLine(xPointer, yPointer, 
+					xPointer + parseInt((distance * rotatorsLengths[i]) * Math.cos(angleRad2)), 
+					yPointer + parseInt((distance * rotatorsLengths[i]) * Math.sin(angleRad2)), 
+					lineWidth, color);	
 			}
-		}
-							
+			
+		angle += ANGLE_ROTATION;			
+				
 		lastPosX = xPointer;
 		lastPosY = yPointer ; 			
 	}
