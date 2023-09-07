55be720253d035059dc6c20ede5a081805d1c840
unknown <eapelroso@gmail.com>
2023-09-07 00:42:36 -0300

    New script added

---
+++ scripts/art-city.js
@@ -0,0 +1,197 @@
+{
+	let width = 0;
+	let height = 0;
+	let lastPosY = 0;
+	let lastPosX = 0;
+	let angle;
+	let CANVAS_ID = "myCanvas"
+	let ENTROPY_1 = 2;
+	let BUILDINGS_COUNT = 0;
+	let MINIMUM_HEIGHT = 40;
+	let MAXIMUM_HEIGHT = 360;
+	let RAD_CONST = 0.0175;
+
+	let lastRender = 0
+
+	let objects = [];
+
+	class Building{
+		constructor(x, y, height, hue = 80){
+			this.x = x;
+			this.y = y;
+			this.height = height;
+			this.hue = hue;
+		}
+	}
+
+	init = () => {
+		randomize();
+		addEvents();
+		drawFrame();
+	}
+
+	getRandomInt = (min, max) => {
+		return Math.floor(Math.random() * max) + min;
+	}
+
+	addEvents = () => {
+		let canvas = document.getElementById(CANVAS_ID);
+
+		canvas.addEventListener('click', e => {
+		  addBuilding(e.offsetX, e.offsetY, getRandomInt(MINIMUM_HEIGHT, MAXIMUM_HEIGHT), getRandomInt(1, 360));
+		}, false);
+	}
+
+	randomize = () => {	
+		angle = getRandomInt(20, 45);
+	}
+
+	drawFrame = () => {
+		let canvas = document.getElementById(CANVAS_ID);
+		if (canvas.getContext){
+			canvas.width = width;
+	  		canvas.height = height;
+			let ctx = canvas.getContext('2d')
+			ctx.fillStyle = "#000";
+			ctx.fillRect(0, 0, canvas.width, canvas.height);
+			ctx.lineWidth = 1;
+			ctx.strokeStyle = '#000000';
+			ctx.strokeRect(0, 0, width, height);
+		}
+	}
+
+	drawBuilding = (building) => {
+		let canvas = document.getElementById(CANVAS_ID);
+		if (canvas.getContext){
+			let ctx = canvas.getContext('2d');
+			ctx.strokeStyle = '#000000';
+
+			let color = `hsl(${building.hue}, ${100}%, ${50}%)`; 
+			let color2 = `hsl(${building.hue}, ${100}%, ${30}%)`; 
+			let color3 = `hsl(${building.hue}, ${100}%, ${70}%)`; 
+	
+			let blockWidthFactor = Math.cos(angle * RAD_CONST) * building.buildingSideAWidth;
+			let blockHeightFactor = Math.sin(angle * RAD_CONST) * building.buildingSideAWidth;
+
+			//Left Face			
+			ctx.fillStyle = color;
+			ctx.beginPath();
+			ctx.moveTo(building.x, building.y); 
+			ctx.lineTo(building.x - blockWidthFactor, building.y - blockHeightFactor);  
+			ctx.lineTo(building.x - blockWidthFactor, building.y - (blockHeightFactor + building.height)); 
+			ctx.lineTo(building.x, building.y - building.height); 
+			ctx.lineTo(building.x, building.y); 
+			ctx.fill();
+			ctx.stroke();
+			
+
+			//Right Face
+			
+			ctx.fillStyle = color2;
+			ctx.beginPath();
+			ctx.moveTo(building.x, building.y); 
+			ctx.lineTo(building.x + blockWidthFactor, building.y - blockHeightFactor);  
+			ctx.lineTo(building.x + blockWidthFactor, building.y - (blockHeightFactor + building.height)); 
+			ctx.lineTo(building.x, building.y - building.height);
+			ctx.lineTo(building.x, building.y); 
+			ctx.fill();
+			ctx.stroke();
+			
+
+			//Top Face
+			ctx.fillStyle = color3;
+			ctx.beginPath();
+			ctx.moveTo(building.x, building.y - building.height);
+			ctx.lineTo(building.x - blockWidthFactor, building.y - (blockHeightFactor + building.height));  
+			ctx.lineTo(building.x, building.y -  ((blockHeightFactor * 2) + building.height)); 
+			ctx.lineTo(building.x + blockWidthFactor, building.y - (blockHeightFactor + building.height)); 
+			ctx.lineTo(building.x, building.y - building.height); 
+			ctx.fill();
+			ctx.stroke();				
+
+			drawWindows(ctx, building);
+		}
+	}
+
+	drawWindows = (ctx, building) => {		
+		let colorLight = `hsl(${232}, ${100}%, ${50}%)`; 				
+		let colorDark = `hsl(${232}, ${100}%, ${20}%)`; 
+		ctx.strokeStyle = '#000000';
+
+		let windowWidth = ((building.buildingSideAWidth - (building.margin * (building.cols + 1))) / building.cols);
+		let windowHeight = ((building.height - (building.margin * (building.rows + 1))) / building.rows);
+		let blockWidthFactor = Math.cos(angle * RAD_CONST) * windowWidth;
+		let blockHeightFactor = Math.sin(angle * RAD_CONST) * windowWidth;
+
+		for (let ix = 0; ix < building.cols; ix++){
+			for (let iy = 0; iy < building.rows; iy++){
+
+				//Left
+				ctx.fillStyle = colorLight;
+				ctx.beginPath();
+				let wx = building.x - (Math.cos(angle * RAD_CONST) * (building.margin + ((building.margin + windowWidth) * ix)));
+				let wy = building.y - (Math.sin(angle * RAD_CONST) * (building.margin + ((building.margin + windowWidth) * ix))) - (building.margin + ((building.margin + windowHeight) * iy));
+				ctx.moveTo(wx, wy); 
+				ctx.lineTo(wx - blockWidthFactor, wy - blockHeightFactor);  
+				ctx.lineTo(wx - blockWidthFactor, wy - (blockHeightFactor + windowHeight)); 
+				ctx.lineTo(wx, wy - windowHeight); 
+				ctx.lineTo(wx, wy); 
+				ctx.fill();
+				ctx.stroke();
+
+				
+				//Right
+				ctx.fillStyle = colorDark;
+				wx = building.x + (Math.cos(angle * RAD_CONST) * (building.margin + ((building.margin + windowWidth) * ix)));
+				wy = building.y - (Math.sin(angle * RAD_CONST) * (building.margin + ((building.margin + windowWidth) * ix))) - (building.margin + ((building.margin + windowHeight) * iy));
+
+				ctx.beginPath();
+				ctx.moveTo(wx, wy); 
+				ctx.lineTo(wx + blockWidthFactor, wy - blockHeightFactor);  
+				ctx.lineTo(wx + blockWidthFactor, wy - (blockHeightFactor + windowHeight)); 
+				ctx.lineTo(wx, wy - windowHeight);
+				ctx.lineTo(wx, wy); 
+				ctx.fill();
+				ctx.stroke();
+			}
+		}
+	}				
+
+	addBuilding = (x, y, height, hue = 80) => {	
+		let building = new Building(x, y, height, hue);		
+		building.rows = getRandomInt(1, Math.floor(height / 20)); 
+		building.cols = getRandomInt(1, 5);		
+		building.margin = getRandomInt(0, 5);
+		building.buildingSideAWidth = getRandomInt(40, 60);
+
+		objects.push(building);
+		BUILDINGS_COUNT++; 
+		console.log(objects);
+	}			
+			
+	draw = () => {	
+		if (BUILDINGS_COUNT > 0)
+			for (let i = 0; i < BUILDINGS_COUNT; i++){
+				this.drawBuilding(objects[i]);
+			}				
+	}
+
+	width = window.innerWidth;
+	height = window.innerHeight;
+	lastPosY = 0;
+	lastPosX = 0;			
+
+	loop = (timestamp) => {
+		let progress = timestamp - lastRender;
+
+		draw();
+
+		lastRender = timestamp;
+		window.requestAnimationFrame(loop);
+	}
+
+	init();
+
+	window.requestAnimationFrame(loop)	
+
+}
