fab1593e65cd3a614d326b39d2668f778587279d
ALGEIBA\epelroso <epelroso@algeiba.com>
2023-09-20 17:59:16 -0300

    Show lines info

--- scripts/art-metro.js
+++ scripts/art-metro.js
@@ -52,7 +52,6 @@
 				else
 					this.symbol = objects[objects.length - 1].symbol + 1;
 
-
 			let lastX = this.x;
 			let lastY = this.y;
 			let lastDirection = 0; 
@@ -161,27 +160,54 @@
 		}, false);
 	}
 
+	getNumberOfStations = () => {
+		let numberOfStations = 0;
+		objects.forEach((element) => numberOfStations += element.stations.length);
+		return numberOfStations;
+	}	
+
+	getNumberOfLines = () => {
+		return objects.length;
+	}	
+
+	getLinesLength = () => {
+		let linesLength = 0;
+		for (let i = 0; i < LINES_COUNT; i++){
+			for (let j = 1; j < objects[i].points.length; j++){
+				linesLength += Math.floor(Math.sqrt(Math.pow(objects[i].points[j].x - objects[i].points[j-1].x, 2) + Math.pow(objects[i].points[j].y - objects[i].points[j-1].y, 2)));
+			}
+		}	
+		return Math.floor(linesLength / 100);
+	}
+
 	drawLinesInfo = (ctx, canvas) => {
 		let marginTop = 30;
 		let marginLeft = 30;
 		let symbolSide = 15;
 		let lineHeight = 20;
+		let headerHeight = 80;
+		let padding = 10;
 		
 		ctx.fillStyle = "#FFF";
 		let frameWidth = 100;
-		let frameHeight = marginTop + LINES_COUNT * lineHeight;
-		ctx.fillRect(marginTop, marginLeft, frameWidth, frameHeight);
+		let frameHeight = marginTop + headerHeight + LINES_COUNT * lineHeight;
+		ctx.fillRect(marginLeft, marginTop, frameWidth, frameHeight);
 		ctx.lineWidth = 1;
 		ctx.strokeStyle = '#000000';
-		ctx.strokeRect(marginTop, marginLeft, frameWidth, frameHeight);
+		ctx.strokeRect(marginLeft, marginTop, frameWidth, frameHeight);
+
+		ctx.font = "10px Arial";
+		ctx.fillStyle = "#000";	
+		ctx.fillText(`City Metro System`, marginLeft + padding, marginTop + padding * 2); 
+		ctx.fillText(`Stations: ${getNumberOfStations()}`, marginLeft + padding, marginTop + padding * 2 + lineHeight); 
+		ctx.fillText(`Lines: ${getNumberOfLines()}`, marginLeft + padding, marginTop + padding * 2 + lineHeight * 2); 
+		ctx.fillText(`Length: ${getLinesLength()} km.`, marginLeft + padding, marginTop + padding * 2 + lineHeight * 3); 
 
 		for (let i = 0; i < LINES_COUNT; i++){
-			drawRectangle(ctx, marginLeft + 10, marginTop + 10 + i * lineHeight, symbolSide, symbolSide, '#000', objects[i].colorBase());
-			ctx.fillStyle = "#000";
-			ctx.font = "10px Arial";
-			ctx.fillText(`Line ${objects[i].symbol}`, marginLeft + symbolSide + 20, marginTop + 20 + i * lineHeight); 
-		}		
-
+			drawRectangle(ctx, marginLeft + padding, marginTop + headerHeight + padding + i * lineHeight, symbolSide, symbolSide, '#000', objects[i].colorBase());
+			ctx.fillStyle = "#000";			
+			ctx.fillText(`Line ${objects[i].symbol}`, marginLeft + symbolSide + padding * 2, marginTop + headerHeight + padding * 2 + i * lineHeight); 
+		}	
 	}
 
 	drawFrame = () => {
