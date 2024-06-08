7c72736ce6164ebf271f4e8d05bfe03e9e53ba13
eapelroso <eapelroso@gmail.com>
2024-06-08 18:52:21 +0000

    Fixed quadtree

--- scripts/art-balls-bouncing.js
+++ scripts/art-balls-bouncing.js
@@ -1,5 +1,6 @@
 {
-	let DRAW_QUADTREE = true;
+
+	let DRAW_QUADTREE = false;
 	
 	let hue = 20;
 	let radius = 20;
@@ -141,155 +142,9 @@
 		draw = (ctx) => {
 			if (this.balls.length > 0) {
 				if (DRAW_QUADTREE)
-					this.quad.drawQuadtree(ctx);
+					this.quad.drawQuadtree(ctx, this.quad);
 				this.drawBalls(ctx);
 				this.populateQuadTree();
-			}
-		}
-	}
-
-	class Rectangle {
-		constructor(x, y, w, h) {
-			this.x = x;
-			this.y = y;
-			this.width = w;
-			this.height = h;
-		}
-
-		getWidth = () => this.width;
-		getHeight = () => this.height;
-		getX = () => this.x;
-		getY = () => this.y;
-		getTop = () => this.y;
-		getBottom = () => this.y + this.height;
-		getLeft = () => this.x;
-		getRight = () => this.x + this.width;
-	}
-
-	class Quadtree {
-		constructor(level, bounds) {
-
-			this.MAX_OBJECTS = 5;
-			this.MAX_LEVELS = 6;
-
-			this.lines = [];
-
-			this.level = level;
-			this.bounds = bounds;
-			this.nodes = new Array(4);
-		}
-
-		clear = () => {
-			this.lines = [];
-
-			for (let i = 0; i < this.nodes.length; i++) {
-				if (this.nodes[i] != null) {
-					this.nodes[i] = null;
-				}
-			}
-		}
-
-		split = () => {
-			let subWidth = Math.floor(this.bounds.getWidth() / 2);
-			let subHeight = Math.floor(this.bounds.getHeight() / 2);
-			let x = this.bounds.getX();
-			let y = this.bounds.getY();
-
-			this.nodes[0] = new Quadtree(this.level + 1, new Rectangle(x + subWidth, y, subWidth, subHeight));
-			this.nodes[1] = new Quadtree(this.level + 1, new Rectangle(x, y, subWidth, subHeight));
-			this.nodes[2] = new Quadtree(this.level + 1, new Rectangle(x, y + subHeight, subWidth, subHeight));
-			this.nodes[3] = new Quadtree(this.level + 1, new Rectangle(x + subWidth, y + subHeight, subWidth, subHeight));
-		}
-
-		getIndex = (rectangle) => {
-			let index = -1;
-			let verticalMidsegment = this.bounds.getX() + (this.bounds.getWidth() / 2);
-			let horizontalMidsegment = this.bounds.getY() + (this.bounds.getHeight() / 2);
-
-			let topQuadrant = (rectangle.getTop() < horizontalMidsegment && rectangle.getBottom() < horizontalMidsegment);
-			let bottomQuadrant = (rectangle.getTop() > horizontalMidsegment);
-
-			if (rectangle.getLeft() < verticalMidsegment && rectangle.getRight() < verticalMidsegment) {
-				if (topQuadrant) {
-					index = 1;
-				}
-				else if (bottomQuadrant) {
-					index = 2;
-				}
-			}
-
-			else if (rectangle.getLeft() > verticalMidsegment) {
-				if (topQuadrant) {
-					index = 0;
-				}
-				else if (bottomQuadrant) {
-					index = 3;
-				}
-			}
-
-			return index;
-		}
-
-		insert = (rectangle) => {
-			if (this.nodes[0] != null) {
-				let index = this.getIndex(rectangle);
-
-				if (index != -1) {
-					this.nodes[index].insert(rectangle);
-
-					return;
-				}
-			}
-
-			this.lines.push(rectangle);
-
-			if (this.lines.length > this.MAX_OBJECTS && this.level < this.MAX_LEVELS) {
-				if (this.nodes[0] == null) {
-					this.split();
-				}
-
-				let i = 0;
-				while (i < this.lines.length) {
-					let index = this.getIndex(this.lines[i]);
-					if (index != -1) {
-						let removedItem = this.lines[i];
-						this.lines.splice(i, 1);
-						this.nodes[index].insert(removedItem);
-					}
-					else {
-						i++;
-					}
-				}
-			}
-		}
-
-		retrieve = (returnObjects, rectangle) => {
-			let index = this.getIndex(rectangle);
-			if (index != -1 && this.nodes[0] != null) {
-				this.nodes[index].retrieve(returnObjects, rectangle);
-			}
-
-			returnObjects.push(...this.lines);
-
-			return returnObjects;
-		}
-
-		static generateQuadtree = (width, height) => {
-			return new Quadtree(0, new Rectangle(0, 0, width, height));
-		}
-
-		drawQuadtree = (ctx) => {
-			if (this.quad != null) {
-				if (this.quad.bounds != null) {
-					ctx.strokeStyle = "#333";
-					ctx.lineWidth = 1;
-					ctx.strokeRect(this.quad.bounds.x, this.quad.bounds.y, this.quad.bounds.width, this.quad.bounds.height);
-				}
-				if (this.quad.nodes != null) {
-					this.quad.nodes.forEach(function (node) {
-						drawQuadtree(ctx, node);
-					});
-				}
 			}
 		}
 	}
--- scripts/art-metro.js
+++ scripts/art-metro.js
@@ -10,7 +10,7 @@
 	let INFO_HEADER_HEIGHT = 100;
 	let INFO_PADDING = 10;
 	let INFO_WIDTH = 120;
-	let DRAW_QUADTREE = true;
+	let DRAW_QUADTREE = false;
 	let LINES_LIMIT = true;
 
 	let maxNumberOfLines = 15;
@@ -119,7 +119,7 @@
 		draw = (ctx) => {
 			if (this.lines.length > 0) {
 				if (DRAW_QUADTREE)
-					this.quad.drawQuadtree(ctx);
+					this.quad.drawQuadtree(ctx, this.quad);
 				this.drawLines(ctx);
 				this.drawLinesInfo(ctx);
 				this.populateQuadTree();
@@ -388,152 +388,6 @@
 		}
 
 		colorBase = () => `hsl(${this.hue}, ${this.saturation}%, ${this.light}%)`;
-	}
-
-	class Quadtree {
-		constructor(level, bounds) {
-
-			this.MAX_OBJECTS = 5;
-			this.MAX_LEVELS = 6;
-
-			this.lines = [];
-
-			this.level = level;
-			this.bounds = bounds;
-			this.nodes = new Array(4);
-		}
-
-		clear = () => {
-			this.lines = [];
-
-			for (let i = 0; i < this.nodes.length; i++) {
-				if (this.nodes[i] != null) {
-					this.nodes[i] = null;
-				}
-			}
-		}
-
-		split = () => {
-			let subWidth = Math.floor(this.bounds.getWidth() / 2);
-			let subHeight = Math.floor(this.bounds.getHeight() / 2);
-			let x = this.bounds.getX();
-			let y = this.bounds.getY();
-
-			this.nodes[0] = new Quadtree(this.level + 1, new Rectangle(x + subWidth, y, subWidth, subHeight));
-			this.nodes[1] = new Quadtree(this.level + 1, new Rectangle(x, y, subWidth, subHeight));
-			this.nodes[2] = new Quadtree(this.level + 1, new Rectangle(x, y + subHeight, subWidth, subHeight));
-			this.nodes[3] = new Quadtree(this.level + 1, new Rectangle(x + subWidth, y + subHeight, subWidth, subHeight));
-		}
-
-		getIndex = (rectangle) => {
-			let index = -1;
-			let verticalMidsegment = this.bounds.getX() + (this.bounds.getWidth() / 2);
-			let horizontalMidsegment = this.bounds.getY() + (this.bounds.getHeight() / 2);
-
-			let topQuadrant = (rectangle.getTop() < horizontalMidsegment && rectangle.getBottom() < horizontalMidsegment);
-			let bottomQuadrant = (rectangle.getTop() > horizontalMidsegment);
-
-			if (rectangle.getLeft() < verticalMidsegment && rectangle.getRight() < verticalMidsegment) {
-				if (topQuadrant) {
-					index = 1;
-				}
-				else if (bottomQuadrant) {
-					index = 2;
-				}
-			}
-
-			else if (rectangle.getLeft() > verticalMidsegment) {
-				if (topQuadrant) {
-					index = 0;
-				}
-				else if (bottomQuadrant) {
-					index = 3;
-				}
-			}
-
-			return index;
-		}
-
-		insert = (rectangle) => {
-			if (this.nodes[0] != null) {
-				let index = this.getIndex(rectangle);
-
-				if (index != -1) {
-					this.nodes[index].insert(rectangle);
-
-					return;
-				}
-			}
-
-			this.lines.push(rectangle);
-
-			if (this.lines.length > this.MAX_OBJECTS && this.level < this.MAX_LEVELS) {
-				if (this.nodes[0] == null) {
-					this.split();
-				}
-
-				let i = 0;
-				while (i < this.lines.length) {
-					let index = this.getIndex(this.lines[i]);
-					if (index != -1) {
-						let removedItem = this.lines[i];
-						this.lines.splice(i, 1);
-						this.nodes[index].insert(removedItem);
-					}
-					else {
-						i++;
-					}
-				}
-			}
-		}
-
-		retrieve = (returnObjects, rectangle) => {
-			let index = this.getIndex(rectangle);
-			if (index != -1 && this.nodes[0] != null) {
-				this.nodes[index].retrieve(returnObjects, rectangle);
-			}
-
-			returnObjects.push(...this.lines);
-
-			return returnObjects;
-		}
-
-		static generateQuadtree = (width, height) => {
-			return new Quadtree(0, new Rectangle(0, 0, width, height));
-		}
-
-		drawQuadtree = (ctx) => {
-			if (this.quad != null) {
-				if (this.quad.bounds != null) {
-					ctx.strokeStyle = "#333";
-					ctx.lineWidth = 1;
-					ctx.strokeRect(this.quad.bounds.x, this.quad.bounds.y, this.quad.bounds.width, this.quad.bounds.height);
-				}
-				if (this.quad.nodes != null) {
-					this.quad.nodes.forEach(function (node) {
-						drawQuadtree(ctx, node);
-					});
-				}
-			}
-		}
-	}
-
-	class Rectangle {
-		constructor(x, y, w, h) {
-			this.x = x;
-			this.y = y;
-			this.width = w;
-			this.height = h;
-		}
-
-		getWidth = () => this.width;
-		getHeight = () => this.height;
-		getX = () => this.x;
-		getY = () => this.y;
-		getTop = () => this.y;
-		getBottom = () => this.y + this.height;
-		getLeft = () => this.x;
-		getRight = () => this.x + this.width;
 	}
 
 	let init = () => {
