099c94d94982f3b18233f67c051c39d0fd47278d
eapelroso <eapelroso@gmail.com>
2025-02-04 17:33:12 +0000

    Fix PWA offline

--- Service_Worker.js
+++ Service_Worker.js
@@ -1,48 +1,56 @@
-const CACHE_NAME = 'creataive-coding-lab',
-    urlsToCache = [
-        '.',
-        './index.html',
-    ]
+const CACHE_NAME = 'creative-coding-lab';
 
-self.addEventListener('install', e => {
+self.addEventListener('install', (e) => {
     e.waitUntil(
         caches.open(CACHE_NAME)
-            .then(cache => {
-                return cache.addAll(urlsToCache)
-                    .then(() => self.skipWaiting())
+            .then((cache) => {
+                return cache.addAll([
+                    '/',
+                    '/index.html',
+                ]);
             })
-            .catch(err => console.log('Falló registro de cache', err))
-    )
-})
+            .then(() => self.skipWaiting())
+    );
+});
 
+self.addEventListener('fetch', (e) => {
+    e.respondWith(
+        caches.match(e.request)
+            .then((cachedResponse) => {
+                if (cachedResponse) {
+                    return cachedResponse;
+                }
+
+                return fetch(e.request)
+                    .then((networkResponse) => {
+                        const responseToCache = networkResponse.clone();
+
+                        caches.open(CACHE_NAME)
+                            .then((cache) => {
+                                cache.put(e.request, responseToCache);
+                            });
+
+                        return networkResponse;
+                    })
+                    .catch(() => {
+                        return caches.match('/index.html');
+                    });
+            })
+    );
+});
 self.addEventListener('activate', e => {
-    const cacheWhitelist = [CACHE_NAME]
+    const cacheWhitelist = [CACHE_NAME];
     e.waitUntil(
         caches.keys()
             .then(cacheNames => {
                 return Promise.all(
                     cacheNames.map(cacheName => {
                         if (cacheWhitelist.indexOf(cacheName) === -1) {
-                            return caches.delete(cacheName)
+                            return caches.delete(cacheName);
                         }
                     })
-                )
+                );
             })
-
             .then(() => self.clients.claim())
-    )
-})
-
-self.addEventListener('fetch', e => {
-    
-    e.respondWith(
-        caches.match(e.request)
-            .then(res => {
-                if (res) {
-                    return res
-                }
-                
-                return fetch(e.request)
-            })
-    )
-})
+    );
+});
