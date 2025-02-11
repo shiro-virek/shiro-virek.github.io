b60cf51c3bef8b7db7007f97f995b9cf4db5db26
eapelroso <eapelroso@gmail.com>
2025-02-11 18:22:51 +0000

    Changes MNCA params

--- scripts/art-cellular-automata-mn.js
+++ scripts/art-cellular-automata-mn.js
@@ -6,8 +6,8 @@
     let cellPadding = 0;
     let cellDiameter = 20;
 
-    let extendedSize = 3;
-    let circleRadius = 3;
+    let extendedSize = 2;
+    let circleRadius = 2;
 
     let cellScreen;
             
@@ -34,7 +34,7 @@
     }
 
     class Rule {
-        constructor(conditionNeighbours, valueNeighbours, value2Neighbours, newValueCell, aliveStatusRequired){
+        constructor(aliveStatusRequired, conditionNeighbours, valueNeighbours, value2Neighbours, newValueCell){
             this.valueNeighbours = valueNeighbours;
             this.value2Neighbours = value2Neighbours;
             this.conditionNeighbours = conditionNeighbours;
@@ -254,7 +254,7 @@
     let init = () => {       
 		initCanvas();
 
-        cellDiameter = Utils.getRandomInt(3, 15);        
+        cellDiameter = Utils.getRandomInt(5, 15);        
 
         cellRows = Math.floor((height - cellMargin)/ (cellDiameter + cellPadding));
         cellColumns = Math.floor((width - cellMargin)/ (cellDiameter + cellPadding));
@@ -278,25 +278,6 @@
         }    
         setRandomRules();
         //setRules();
-    }
-
-    let setRules = () => {
-        //Conway rules
-        
-        let neighborhood = new Neighborhood();
-        neighborhood.neighborhoodType = NeighborhoodType.VonNeumann;
-
-        let rule1 = new Rule(Condition.Lower, 2, 0, false, true);
-        let rule2 = new Rule(Condition.Between, 2, 3, true, true);
-        let rule3 = new Rule(Condition.Greater, 3, 0, false, true);
-        let rule4 = new Rule(Condition.Equal, 3, 0, true, false);
-
-        neighborhood.rules.push(rule1);
-        neighborhood.rules.push(rule2);
-        neighborhood.rules.push(rule3);
-        neighborhood.rules.push(rule4);
-
-        cellScreen.neighborhoods.push(neighborhood);
     }
 
     let getRandomRule = (neighborhood) => {
@@ -330,7 +311,7 @@
         let newValueCell = Utils.getRandomBool();  
         let alive = Utils.getRandomBool();
 
-        return new Rule(condition, valueNeighbours, value2Neighbours, newValueCell, alive);
+        return new Rule(alive, condition, valueNeighbours, value2Neighbours, newValueCell);
     }
 
     let setRandomRules = () => {
@@ -366,7 +347,7 @@
 
         draw();
 
-        Utils.sleep(300);
+        Utils.sleep(200);
 
         lastRender = timestamp;
         window.requestAnimationFrame(loop);
