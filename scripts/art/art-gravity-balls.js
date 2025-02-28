72c196c64754eb7231ae549040ab66e50f464883
Emanuel Pelroso <eapelroso@gmail.com>
2025-02-28 14:12:16 +0000

    Multiple balls

--- scripts/art/art-gravity-balls.js
+++ scripts/art/art-gravity-balls.js
@@ -14,56 +14,67 @@
 
 	const globals = {
 		random: null,
-        ball: null,
+        balls: [],
 	}
 
-    function drawBall() {
-        ctx.beginPath();
-        ctx.arc(globals.ball.x, globals.ball.y, config.ballRadius, 0, Math.PI * 2);
-        ctx.fillStyle = '#007bff';
-        ctx.fill();
-        ctx.closePath();
+    class Ball {
+        constructor(x, y) {
+			this.x = x;
+			this.y = y;
+			this.radius = 20;
+            this.dy = 0;
+
+		}
+
+        draw = () => {
+            ctx.beginPath();
+            ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
+            ctx.fillStyle = '#007bff';
+            ctx.fill();
+            ctx.closePath();
+        }
     }
+
+  
 
 	let init = () => {
 		initCanvas();
 
-        globals.ball = {
-            x: canvas.width / 2,
-            y: config.ballRadius,
-            dy: 0
-        };
+        let ball = new Ball(canvas.width / 2, config.ballRadius);
+
+        globals.balls.push(ball);     
         
 		window.requestAnimationFrame(loop);
 	}
 
     let draw = () => {
         drawBackground(ctx, canvas);
-        drawBall();
     }
 
 	let loop = (timestamp) => {
 		let progress = timestamp - lastRender;
 
-        globals.ball.dy += config.gravity;
-        globals.ball.y += globals.ball.dy;
-
-        if (globals.ball.y + config.ballRadius > canvas.height) {
-            globals.ball.y = canvas.height - config.ballRadius;
-            globals.ball.dy = -globals.ball.dy * config.damping; 
-
-            if (Math.abs(globals.ball.dy) < 1) {
-                globals.ball.dy = 0;
-            }
-        }
-
         draw();
 
+        for (const ball of globals.balls) {
+            ball.dy += config.gravity;
+            ball.y += ball.dy;
+    
+            if (ball.y + ball.radius > canvas.height) {
+                ball.y = canvas.height - ball.radius;
+                ball.dy = -ball.dy * config.damping; 
+    
+                if (Math.abs(ball.dy) < 1) {
+                    ball.dy = 0;
+                }
+            }
+
+            ball.draw();
+        }
+        
 		lastRender = timestamp;
 		
-        if (globals.ball.dy !== 0) {
-            requestAnimationFrame(loop);
-        }
+        requestAnimationFrame(loop);
 	}
 
 	init();
