943037b9ac606d2bf4bb4328203fecb559909660
Emanuel Pelroso <eapelroso@gmail.com>
2025-06-10 14:57:39 +0000

    Pixels methods

--- scripts/art/art-fractals.js
+++ scripts/art/art-fractals.js
@@ -27,6 +27,15 @@
 
     let draw = () => {
         drawBackground(ctx, canvas);
+        Pixels.setSinglePixel(ctx, 20, 20, '#FFF');
+        Pixels.setSinglePixel2(ctx, 30, 30, '#FFF');
+
+        const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+        const data = imageData.data;
+
+        Pixels.setPixelBatch(ctx, data, 40, 40, 255, 255, 255);
+
+        ctx.putImageData(imageData, 0, 0);
     }
 
     let loop = (timestamp) => {
