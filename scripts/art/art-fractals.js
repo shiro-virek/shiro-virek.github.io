701cf531f81de15d294b96ab6cb7a93e35003328
Emanuel Pelroso <eapelroso@gmail.com>
2025-06-11 12:32:28 +0000

    Basic mandelbrot

--- scripts/art/art-fractals.js
+++ scripts/art/art-fractals.js
@@ -6,7 +6,28 @@
     const config = {
         randomize: true,
     };    
-    
+
+ 
+
+    let isInMandelbrotSet = (rc, ic, maxIterations = 50) => {
+        let zr = 0;
+        let zi = 0;
+        
+        for (let iterations = 0; iterations < maxIterations; iterations++) {
+            const zrNew = zr * zr - zi * zi + rc;
+            const ziNew = 2 * zr * zi + ic;
+            
+            zr = zrNew;
+            zi = ziNew;
+            
+            if (zr * zr + zi * zi > 4) {
+                return false;
+            }
+        }
+        
+        return true;
+    }
+
     let init = () => {
 		globals.random = Objects.getRandomObject();
         initCanvas();
@@ -27,13 +48,26 @@
 
     let draw = () => {
         drawBackground(ctx, canvas);
-        Pixels.setSinglePixel(ctx, 20, 20, '#FFF');
-        Pixels.setSinglePixel2(ctx, 30, 30, '#FFF');
 
         const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
         const data = imageData.data;
+            
+        const scale = 0.005;
+        const offsetX = -width * scale / 2;
+        const offsetY = -height * scale / 2;
+        
+        for (let x=0; x < width; x++){
+            for (let y=0; y < height; y++){
+                const rc = x * scale + offsetX;
+                const ic = y * scale + offsetY;
 
-        Pixels.setPixelBatch(ctx, data, 40, 40, 255, 255, 255);
+                if (isInMandelbrotSet(rc, ic)){
+                    Pixels.setPixelBatch(ctx, data, x, y, 255, 255, 255);
+                }else{
+                    Pixels.setPixelBatch(ctx, data, x, y, 0, 0, 0);
+                }                
+            }
+        }        
 
         ctx.putImageData(imageData, 0, 0);
     }
