efa5bc9c42d426a8060f29563152d412e6c33313
Emanuel Pelroso <eapelroso@gmail.com>
2025-06-13 18:30:15 +0000

    Fire trail

--- scripts/utils/objects.js
+++ scripts/utils/objects.js
@@ -3,6 +3,11 @@ class Objects {
         return JSON.parse(JSON.stringify(original));
     }
 
+    static cloneWithMethods = (original) => {
+        const copy = Object.create(Object.getPrototypeOf(original));
+        return Object.assign(copy, original);
+    }
+
     static getRandomObject = () => {
         if (!Url.hasUrlParam('seed')){
 			Url.setUrlParam('seed', Date.now());
