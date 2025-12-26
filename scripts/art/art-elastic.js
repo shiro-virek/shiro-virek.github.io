cbabf7149e4f9551a6d875fc76be4bd297494b09
Emanuel Pelroso <eapelroso@gmail.com>
2025-12-26 17:19:05 +0000

    Wip

---
+++ scripts/art/art-elastic.js
@@ -0,0 +1,47 @@
+{
+    const globals = {
+        random: null
+    };
+
+    const config = {
+        randomize: true,
+    };    
+
+    let init = () => {
+		globals.random = Objects.getRandomObject();
+        if (config.randomize) randomize();
+        initCanvas();
+        addEvents();
+        window.requestAnimationFrame(loop);
+    }
+
+    let addEvents = () => {
+    }
+
+    let randomize = () => {
+    }
+    
+    window.draw = () => {
+        drawBackground(ctx, canvas);
+    }
+
+    window.trackMouse = (x, y) => {
+        if (clicking) {  
+
+        }
+    }
+    
+    window.clearCanvas = () => {
+		Sound.error();
+    }
+
+	window.magic = () => {  
+		Sound.error();
+	}
+
+    window.upload = (e) => {
+		Sound.error();        
+    }
+
+    init();
+}
