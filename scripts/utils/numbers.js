5b7b6cca44d0a5d4ca33c75473b1730fd511b294
Emanuel Pelroso <eapelroso@gmail.com>
2026-02-25 16:33:30 -0300

    Added easing functions

--- scripts/utils/numbers.js
+++ scripts/utils/numbers.js
@@ -25,4 +25,27 @@ class Numbers {
         im: rP * Math.sin(thetaP)
         };
     }
+
+    static easings = {
+        linear: (t) => t,
+        easeInQuad: (t) => t * t,
+        easeOutQuad: (t) => t * (2 - t),
+        easeInOutQuad: (t) => t < 0.5 ? 2 * t * t : -1 + (4 - 2 * t) * t,
+        easeInCubic: (t) => t * t * t,
+        easeOutCubic: (t) => (--t) * t * t + 1,
+        easeInOutCubic: (t) => t < 0.5 ? 4 * t * t * t : (t - 1) * (2 * t - 2) * (2 * t - 2) + 1,
+        easeInQuart: (t) => t * t * t * t,
+        easeOutQuart: (t) => 1 - (--t) * t * t * t,
+        easeInOutQuart: (t) => t < 0.5 ? 8 * t * t * t * t : 1 - 8 * (--t) * t * t * t,
+        easeInBack: (t) => {
+            const c1 = 1.70158;
+            const c3 = c1 + 1;
+            return c3 * t * t * t - c1 * t * t;
+        },
+        easeOutBack: (t) => {
+            const c1 = 1.70158;
+            const c3 = c1 + 1;
+            return 1 + c3 * Math.pow(t - 1, 3) + c1 * Math.pow(t - 1, 2);
+        }
+    };
 }
