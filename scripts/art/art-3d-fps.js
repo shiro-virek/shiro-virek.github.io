2fca3885dca49181783ae3f40cc0d56c7a33ced6
Emanuel Pelroso <eapelroso@gmail.com>
2026-03-27 10:30:21 -0300

    Change floor

--- scripts/art/art-3d-fps.js
+++ scripts/art/art-3d-fps.js
@@ -309,7 +309,7 @@
         }
 
         translateInfiniteFloor = () => {    
-            if (this.cameraX < globals.floorCenterX - config.tileSize && this.cameraX > -config.worldSize + config.tileSize * 4) {
+            if (this.cameraX < globals.floorCenterX - config.tileSize && this.cameraX > -config.worldSize + config.tileSize * (config.floorSize / 1000 * 2)) {
                 globals.world.figures.filter(face => face.infinite).forEach(face => {    
                     face.translateX(-config.tileSize);          
                 });
@@ -317,7 +317,7 @@
                 globals.floorCenterX -= config.tileSize;
             }    
                         
-            if (this.cameraX > globals.floorCenterX + config.tileSize && this.cameraX < config.worldSize - config.tileSize * 4) {
+            if (this.cameraX > globals.floorCenterX + config.tileSize && this.cameraX < config.worldSize - config.tileSize * (config.floorSize / 1000 * 2)) {
                 globals.world.figures.filter(face => face.infinite).forEach(face => {    
                     face.translateX(config.tileSize);          
                 });
@@ -325,7 +325,7 @@
                 globals.floorCenterX += config.tileSize;
             }    
 
-            if (this.cameraZ < globals.floorCenterZ - config.tileSize && this.cameraZ > -config.worldSize + config.tileSize * 4) {
+            if (this.cameraZ < globals.floorCenterZ - config.tileSize && this.cameraZ > -config.worldSize + config.tileSize * (config.floorSize / 1000 * 2)) {
                 globals.world.figures.filter(face => face.infinite).forEach(face => {    
                     face.translateZ(-config.tileSize);          
                 });
@@ -333,7 +333,7 @@
                 globals.floorCenterZ -= config.tileSize;
             }    
                         
-            if (this.cameraZ > globals.floorCenterZ + config.tileSize && this.cameraZ < config.worldSize - config.tileSize * 4) {
+            if (this.cameraZ > globals.floorCenterZ + config.tileSize && this.cameraZ < config.worldSize - config.tileSize * (config.floorSize / 1000 * 2)) {
                 globals.world.figures.filter(face => face.infinite).forEach(face => {    
                     face.translateZ(config.tileSize);          
                 });
