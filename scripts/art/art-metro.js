1e7b950296f329d17ed5d9c440c6f6b34cae0ecb
Emanuel Pelroso <eapelroso@gmail.com>
2026-07-11 15:38:46 -0300

    Method moved

--- scripts/art/art-metro.js
+++ scripts/art/art-metro.js
@@ -641,20 +641,13 @@
 			for (let i = 1; i < line.segments.length; i++) {
 				let s1 = line.segments[i - 1];
 				let s2 = line.segments[i];
-				let dist = distanceBetweenSegments(x1, y1, x2, y2, s1.x, s1.y, s2.x, s2.y);
+				let dist = Trigonometry.distanceBetweenSegments(x1, y1, x2, y2, s1.x, s1.y, s2.x, s2.y);
 				if (dist < threshold) return true;
 			}
 		}
 		return false;
 	}
 		
-	function distanceBetweenSegments(x1, y1, x2, y2, x3, y3, x4, y4) {
-		let d1 = Math.hypot(x1 - x3, y1 - y3);
-		let d2 = Math.hypot(x2 - x4, y2 - y4);
-		let d3 = Math.hypot(x1 - x4, y1 - y4);
-		let d4 = Math.hypot(x2 - x3, y2 - y3);
-		return Math.min(d1, d2, d3, d4);
-	}
 
 	let randomize = () => {
 		globals.random = Objects.getRandomObject();
--- scripts/utils/trigonometry.js
+++ scripts/utils/trigonometry.js
@@ -56,6 +56,14 @@ class Trigonometry {
         return Math.sqrt(deltaX * deltaX + deltaY * deltaY);
     }
 
+	static distanceBetweenSegments = (x1, y1, x2, y2, x3, y3, x4, y4) => {
+		let d1 = Math.hypot(x1 - x3, y1 - y3);
+		let d2 = Math.hypot(x2 - x4, y2 - y4);
+		let d3 = Math.hypot(x1 - x4, y1 - y4);
+		let d4 = Math.hypot(x2 - x3, y2 - y3);
+		return Math.min(d1, d2, d3, d4);
+	}
+
     static sexagesimalToRadian = (degrees) => {
         return degrees * (Math.PI / 180);
     }
