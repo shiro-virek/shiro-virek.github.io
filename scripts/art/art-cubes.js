0b8b7739f919c888cd61a5b8b7169e60530831f3
Emanuel Pelroso <eapelroso@gmail.com>
2026-07-21 22:18:17 -0300

    Cubes WIP

--- scripts/art/art-cubes.js
+++ scripts/art/art-cubes.js
@@ -18,8 +18,8 @@
         window.requestAnimationFrame(loop);        
 
         let side = 100;
-        let rows = Math.floor(globals.world.width / side);
-        let columns = Math.floor(globals.world.height / side);
+        let rows = Math.floor(globals.world.width / side) + 1;
+        let columns = Math.floor(globals.world.height / side) + 1;
         for (let x = 0; x < rows; x++){
             for (let z = 0; z < columns; z++){            
                 let figure = globals.world.addFigure(x * side, z * side);
