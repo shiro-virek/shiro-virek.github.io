a2b5a3ba8bffd051d701ae1e0d8dce61707ec3ac
Emanuel Pelroso <eapelroso@gmail.com>
2026-08-14 18:43:19 -0300

    Fixed instances

--- scripts/art/art-tetris.js
+++ scripts/art/art-tetris.js
@@ -1,6 +1,7 @@
 {
     const globals = {
-        random: null
+        random: null,
+        world: null,
     };
 
     const config = {
@@ -42,19 +43,74 @@
             [1, 1, 1]
         ];
 
-        static pieces = [piece1, piece2, piece3, piece4, piece5, piece6, piece7];
+        static pieces = [Tetris.piece1, Tetris.piece2, Tetris.piece3, Tetris.piece4, Tetris.piece5, Tetris.piece6, Tetris.piece7];
 
-        constructor () {
+        newPiece = () => {
             this.piece = Tetris.pieces[globals.random.nextInt(0, Tetris.pieces.length)];
-            this.x = 0;
-            this.y = 0;
+
+            let side = 40;
+            let offset = ((this.piece.length - 1) * side) / 2;
+
+            for (let x = 0; x < this.piece.length; x++){
+                for (let y = 0; y < this.piece[x].length; y++){
+                    if (this.piece[x][y] === 1){
+                        //this.addBlock(x, y);
+                        let figure = globals.world.addFigureAt(x * side - offset, 50, y * side - offset);
+                    }
+                }
+            }
+
         }
+
+        drawPiece = () => {
+        }
+
+        drawBoard = () => {
+
+        }
+        
+        constructor () {
+            this.board = [];
+            this.newPiece();
+        }
+        
     }
 
+    let drawFace = (vertices, lightness, hue, alpha) => {
+        const color = `hsla(${hue}, 100%, ${lightness}%, ${alpha})`;
+        
+        ctx.beginPath();
+        let screenPoint = globals.world.worldToScreen(vertices[0]);
+        ctx.moveTo(screenPoint[0], screenPoint[1]);
+        
+        for (let i = 1; i < vertices.length; i++) {
+            screenPoint = globals.world.worldToScreen(vertices[i]);
+            ctx.lineTo(screenPoint[0], screenPoint[1]);
+        }
+        
+        ctx.closePath();
+        ctx.fillStyle = color;
+        ctx.strokeStyle = color; 
+        ctx.lineWidth = 1;    
+        ctx.fill();
+        ctx.stroke();
+    }
+    
 
     let init = () => {
 		globals.random = Objects.getRandomObject();
         if (config.randomize) randomize();
+        
+        globals.world = new Open3DWorld(width, height, globals.random, Drawing.drawLine, Drawing.drawDot, drawFace);
+
+        globals.world.cameraY = -300;
+        globals.world.cameraZ = -700;
+        globals.world.cameraRotationX = -30;
+
+        globals.world.orbitCamera(0, 0);
+
+		this.tetris = new Tetris();
+
         initCanvas();
         addEvents();
         window.requestAnimationFrame(loop);
@@ -68,6 +124,8 @@
     
     window.draw = (delta) => {
         drawBackground(ctx, canvas);
+
+        globals.world.draw();
     }
 
     window.trackMouse = (x, y) => {
